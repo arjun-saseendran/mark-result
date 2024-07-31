78f5d54b8dd52261da1fc8fb94a0e7914ab96344
arjun-saseendran <arjunsaseendran@icloud.com>
2024-07-31 16:06:27 +0530

    error handling

--- scripts/script.js
+++ scripts/script.js
@@ -4,43 +4,52 @@ let buttonClick = document.querySelector('button');
 buttonClick.addEventListener('click', () => {
 
     let mark = parseInt(inputData.value)
+    let invalid = false;
 
-    if (mark >= 90) {
+    if(mark < 0 || mark > 100){
+        inputData.style.color = 'Orange'
+        inputData.style.textAlign = 'center'
+        inputData.value = 'Enter your mark!!!'
+        invalid = true;
+    }
+
+    if (mark >= 90 && !invalid) {
         inputData.style.color = 'Green'
         inputData.value = 'You got A+ grade!'
-    } else if (mark >= 80) {
+        
+    } else if (mark >= 80 && !invalid) {
         inputData.style.color = 'Green'
         inputData.style.textAlign = 'center'
         inputData.value = 'You got A grade!'
-    } else if (mark >= 70) {
+    } else if (mark >= 70 && !invalid) {
         inputData.style.color = 'Green'
         inputData.style.textAlign = 'center'
         inputData.value = 'You got B+ grade!'
-    } else if (mark >= 60) {
+    } else if (mark >= 60 && !invalid) {
         inputData.style.color = 'Green'
         inputData.style.textAlign = 'center'
         inputData.value = 'You got B grade!'
-    } else if (mark >= 50) {
+    } else if (mark >= 50 && !invalid) {
         inputData.style.color = 'Green'
         inputData.style.textAlign = 'center'
         inputData.value = 'You got C+ grade!'
-    } else if (mark >= 40) {
+    } else if (mark >= 40 && !invalid) {
         inputData.style.color = 'Green'
         inputData.style.textAlign = 'center'
         inputData.value = 'You got C grade!'
-    } else if (mark >= 30) {
+    } else if (mark >= 30 && !invalid) {
         inputData.style.color = 'Green'
         inputData.style.textAlign = 'center'
         inputData.value = 'You got D+ grade!'
-    } else if (mark >= 20) {
+    } else if (mark >= 20 && !invalid) {
         inputData.style.color = 'Red'
         inputData.style.textAlign = 'center'
         inputData.value = 'D grade! You failed!'
-    } else if (mark < 20) {
+    } else if (mark < 20 && !invalid) {
         inputData.style.color = 'Red'
         inputData.style.textAlign = 'center'
         inputData.value = 'E grade! You failed!'
-    } else {
+    }else {
         inputData.style.color = 'Orange'
         inputData.style.textAlign = 'center'
         inputData.value = 'Enter your mark!!!'
@@ -52,8 +61,8 @@ buttonClick.addEventListener('click', () => {
         buttonClick.innerText = 'Submit'
         inputData.value = null;
     }
-
 });
+
 
 
 
